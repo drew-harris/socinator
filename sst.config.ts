9e137641beba5ef9d995ac9f79a7cb310d955db7
Drew Harris <harrisd@smu.edu>
2024-11-05 13:24:10 -0600

    remove db

--- sst.config.ts
+++ sst.config.ts
@@ -14,42 +14,15 @@ export default $config({
     };
   },
   async run() {
-    const dbHost = new sst.Secret("DB_HOST");
-    const dbPass = new sst.Secret("DB_PASS");
     const redshiftUser = new sst.Secret("REDSHIFT_USER");
     const redshiftPassword = new sst.Secret("REDSHIFT_PASSWORD");
     const redshiftHost = new sst.Secret("REDSHIFT_HOST");
     const redshiftDatabase = new sst.Secret("REDSHIFT_DATABASE");
 
-    const dbProvider = new postgresql.Provider("drew-db-provider", {
-      host: dbHost.value,
-      password: dbPass.value,
-      port: 5433,
-      username: "postgres",
-      superuser: true,
-      sslmode: "disable",
-    });
-
-    sst.Linkable.wrap(postgresql.Database, (db) => ({
-      properties: {
-        dbUrl: $interpolate`postgresql://postgres:${dbProvider.password}@${dbProvider.host}:${5433}/${db.name}`,
-      },
-    }));
-
-    const db = new postgresql.Database(
-      "db",
-      {
-        name: $interpolate`socinator_db_${$app.stage}`,
-      },
-      {
-        provider: dbProvider,
-      },
-    );
-
     const server = new sst.aws.Function("Server", {
       url: true,
       handler: "server/src/server.handler",
-      link: [db, redshiftUser, redshiftPassword, redshiftHost, redshiftDatabase],
+      link: [redshiftUser, redshiftPassword, redshiftHost, redshiftDatabase],
     });
 
     const site = new sst.aws.StaticSite("Site", {
